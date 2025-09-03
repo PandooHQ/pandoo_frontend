f0b6ab31389d2c93b6d6331b7023931cff375ffa
newneo4 <noel123mch@gmail.com>
2025-09-02 21:33:32 -0500

    initia configuration

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function NamePage() {
+  return (
+    <div>
+      <h1>Pandoo Home</h1>
+      <Button>Prueba</Button>
+    </div>
+  );
+}
---
+++ src/features/home/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function NamePage() {
+  return (
+    <div>
+      <h1>Hello Page</h1>
+      <Button>Prueba</Button>
+    </div>
+  );
+}
