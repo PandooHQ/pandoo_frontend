6c5fe5aa0bc91c02c91da311e37e0060c222a9a4
Andres Machaca <andres.machaca@comunidadfeliz.cl>
2025-09-29 18:07:15 -0500

    Update deleteForm API endpoint to include '/admin' prefix.

--- src/features/forms/services/deleteForm.ts
+++ src/features/forms/services/deleteForm.ts
@@ -1,7 +1,5 @@
 import api from "@/shared/api/api"
 
 export const deleteForm = async(id:number)=>{
-    const resp = await api.delete(`/forms/${id}`)
-
-    return resp;
+    return await api.delete(`/admin/forms/${id}`);
 }
